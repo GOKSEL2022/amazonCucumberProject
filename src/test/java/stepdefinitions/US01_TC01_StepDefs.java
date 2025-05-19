3abac0ee04a3aa7de9940a063e0727bec6ce22ef
Göksel Celik <gokselcelik5255@gmail.com>
2025-05-20 02:28:57 +0300

    20.05.2025

--- src/test/java/stepdefinitions/US01_TC01_StepDefs.java
+++ src/test/java/stepdefinitions/US01_TC01_StepDefs.java
@@ -1,19 +1,17 @@
 package stepdefinitions;
-
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
+import io.cucumber.java.en.*;
+import utilities.Driver;
 public class US01_TC01_StepDefs {
     @Given("kullanici amazon url ye gider")
     public void kullaniciAmazonUrlYeGider() {
+        Driver.getDriver().get("amazon_url");
     }
-
     @When("kullanici anasayfada oldugunu dogrular")
     public void kullaniciAnasayfadaOldugunuDogrular() {
+        assert Driver.getDriver().getTitle().contains("Amazon");
     }
-
     @Then("kullanici sayfayi kapatir")
     public void kullaniciSayfayiKapatir() {
+        Driver.closeDriver();
     }
 }
