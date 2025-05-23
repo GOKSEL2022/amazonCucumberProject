e24124a55254229890a1e93dd916f95f24052959
Göksel Celik <gokselcelik5255@gmail.com>
2025-05-24 01:09:03 +0300

    24.05.2025

--- src/test/java/pages/AllPages.java
+++ src/test/java/pages/AllPages.java
@@ -1,4 +1,14 @@
 package pages;
 
 public class AllPages {
+    public AllPages() {
+    }
+    private HomePage homePage;
+
+    public HomePage homePage() {
+        if (homePage == null) {
+            homePage = new HomePage();
+        }
+        return homePage;
+    }
 }
