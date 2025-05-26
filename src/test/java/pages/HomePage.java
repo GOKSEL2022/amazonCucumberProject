56e8c4512dc4482ac9132faa6ccf10defd7331fa
Göksel Celik <gokselcelik5255@gmail.com>
2025-05-26 22:50:54 +0300

    26.05.2025

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,14 +1,15 @@
 package pages;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
-
 public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
     @FindBy(id = "nav-link-accountList-nav-line-1")
     public WebElement merhabaGirisYapinHesapVeListelerTextTitleHome;
+
+    @FindBy(xpath = "//*[text()='Giriş yap']")
+    public WebElement girisYapButonHome;
 }
